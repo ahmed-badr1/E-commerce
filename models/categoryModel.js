2f3d10f64f810576cbc8e4198dbd9e258637f836
ahmed <ahmed@localhost.localdomain>
2024-10-09 10:09:19 +0300

    Editing The Private API Keys

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,4 +1,4 @@
-const PRODUCTS_API = process.evn.PRODUCTS_API;
+const PRODUCTS_API = process.evn.REACT_APP_PRODUCTS_API;
 
 export async function fetchProductsByCategory(param) {
   const response = await fetch(`${PRODUCTS_API}/category/${param}`);
--- models/resetPassModel.js
+++ models/resetPassModel.js
@@ -1,4 +1,4 @@
-const AUTH_API = process.evn.AUTHENTICATION_API;
+const AUTH_API = process.evn.REACT_APP_AUTHENTICATION_API;
 
 export async function fetchData(requestInfo) {
   const response = await fetch(`${AUTH_API}/Auth/ResetPassword`, requestInfo);
